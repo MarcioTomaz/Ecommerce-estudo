9caa92f704f21b0bfaeba796feda31ee309757bc
Marcio <marciotmaz@gmail.com>
2025-06-02 13:58:31 -0300

    Adicionando flyway e migration de criação das tabelas e insert da tabela currency

---
+++ EcommerceV3/src/main/resources/db/migration/V2__INSERT_Currency.sql
@@ -0,0 +1,4 @@
+INSERT INTO _currency (active, creation_date, code, name, symbol)
+VALUES (1, CURRENT_DATE, 'BRL', 'Real', 'R$'),
+       (1, CURRENT_DATE, 'USD', 'Dólar', '$'),
+       (1, CURRENT_DATE, 'GBP', 'Libra', '£');
